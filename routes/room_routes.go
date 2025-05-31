4ce75d4056bcfaa232583860d679db79dc940122
sbirmecha99 <shreyabirmecha8@gmail.com>
2025-05-31 10:52:31 +0530

    route protection

--- routes/room_routes.go
+++ routes/room_routes.go
@@ -2,21 +2,24 @@ package routes
 import(
 	"instituteconnect/handlers"
 	"github.com/gofiber/fiber/v2"
+	"instituteconnect/middleware"
 )
 
 func UserRoutes(app *fiber.App) {
 	group := app.Group("/api/users")
-	group.Get("/", handlers.GetUsers)
-	group.Get("/:id", handlers.GetUser)
+
+	group.Get("/", middleware.JWTProtected(),handlers.GetUsers)
+	group.Get("/:id",middleware.JWTProtected(), handlers.GetUser)
 	group.Post("/", handlers.CreateUser)
-	group.Put("/:id", handlers.UpdateUser)
-	group.Delete("/:id", handlers.DeleteUser)
+	group.Put("/:id", middleware.JWTProtected(),handlers.UpdateUser)
+	group.Delete("/:id",middleware.JWTProtected(), handlers.DeleteUser)
 }
 func RoomRoutes(app *fiber.App) {
 	group := app.Group("/api/rooms")
-	group.Get("/", handlers.GetRooms)
-	group.Get("/:id", handlers.GetRoom)
-	group.Post("/", handlers.CreateRoom)
-	group.Put("/:id", handlers.UpdateRoom)
-	group.Delete("/:id", handlers.DeleteRoom)
+
+	group.Get("/",handlers.GetRooms)
+	group.Get("/:id",handlers.GetRoom)
+	group.Post("/", middleware.JWTProtected(),handlers.CreateRoom)
+	group.Put("/:id",middleware.JWTProtected(), handlers.UpdateRoom)
+	group.Delete("/:id",middleware.JWTProtected(), handlers.DeleteRoom)
 }
