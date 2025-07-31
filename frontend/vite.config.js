631244a0678a276a425c2c763632167aeceab391
sbirmecha99 <shreyabirmecha8@gmail.com>
2025-07-31 23:11:29 +0530

    404 vercel issue resolved

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    historyApiFallback:true,
+  build: {
+    outDir: "dist",
+  },
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
+  server: {
+    port: 5173,
   },
 });
